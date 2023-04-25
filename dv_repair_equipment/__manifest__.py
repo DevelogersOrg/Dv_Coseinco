743d899865d15fba37f6039a5d36ddc15627ac7c
renzofbn <renzofbn@gmail.com>
2023-04-25 16:54:12 -0500

    Update __manifest__.py

--- dv_repair_equipment/__manifest__.py
+++ dv_repair_equipment/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name" : "Reparaciones - Coseinco SA",
-    "version" : "14.0.1.0",
+    "version" : "14.0.1.0.0",
     "category" : "",
     "depends" : ['purchase', 'stock', 'repair', 'sale_management', 'crm', 'account', 'point_of_sale'],
     "author": "develogers",
