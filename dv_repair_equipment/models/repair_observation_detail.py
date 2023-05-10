d9affcdf956c0c08cc736ea319c871ec0b7bb095
renzofbn <renzofbn@tutanota.com>
2023-05-10 09:21:53 -0500

    FIX🔧 Ticket R016 aspc. 1

--- dv_repair_equipment/models/repair_observation_detail.py
+++ dv_repair_equipment/models/repair_observation_detail.py
@@ -5,5 +5,10 @@ class RepairObservationDetail(models.Model):
 
   crm_lead_id = fields.Many2one('crm.lead')
   name = fields.Char(string="Observación", required=True)
-  date_of_observation = fields.Datetime(string="Fecha de observación", default=fields.Datetime.now, readonly=True)
+  date_of_observation = fields.Datetime(string="Fecha de observación", readonly=True)
   details = fields.Text(string="Detalles")
+
+  @api.model
+  def create(self, vals):
+      vals['date_of_observation'] = fields.Datetime.now()
+      return super(RepairObservationDetail, self).create(vals)
